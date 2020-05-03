711f61ee541ef715fc61ac39f9579be6857bfaef
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-05-03 17:45:08 +0200

    Changed font

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
 </template>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,300;0,700;1,400&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap");
 
 * {
-  font-family: "Exo 2", sans-serif;
+  font-family: "Roboto", sans-serif;
   box-sizing: border-box;
   margin: 0px;
   padding: 0px;
