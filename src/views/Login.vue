ba886b99754e0eb5985079241ed7fbaf234e4c14
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2020-05-05 09:40:25 +0200

    cleaned login layout a bit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,11 @@
 <template>
-  <v-app class="bg-dark">
-    <v-content>
+  <v-app>
+    <v-content class="grey darken-4">
       <v-container fill-height>
         <v-layout align-center justify-center>
           <v-flex xs9 sm7 md5 lg4>
             <v-card light class="pa-10 text-center">
-              <v-card-title class="text-wrap"
-                >Log in to your account</v-card-title
-              >
+              <v-card-title>Log in to your account</v-card-title>
               <v-form class="mt-4">
                 <v-text-field
                   light
@@ -20,7 +18,7 @@
                   label="Password"
                   prepend-icon="mdi-lock"
                 />
-                <v-btn large class="d-flex w-100 mt-6">Log in</v-btn>
+                <v-btn large width="100%" class="mt-6" dark>Log in</v-btn>
               </v-form>
             </v-card>
           </v-flex>
@@ -29,16 +27,6 @@
     </v-content>
   </v-app>
 </template>
-
-<style scoped>
-.bg-dark {
-  background: #333;
-}
-
-.w-100 {
-  width: 100%;
-}
-</style>
 
 <script>
 export default {
