7ffa370c3f66c69ab3cd541caca4cc17ddafd60b
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-10-11 17:04:48 +0200

    Assignment service implemented

---
+++ src/services/assignment.service.js
@@ -0,0 +1,208 @@
+export default {
+  async getAssignments({id, external, year, name}, notify) {
+    const response = await fetch(`/services/assignments.php?action=getAssignments&course_id=${id}${external ? "&X" : ""}&year=${year}`, {
+      method: "get",
+      headers: {
+        Accept: "application/json"
+      }
+    });
+    const body = await response.json();
+    if (!body.success) {
+      notify({
+        type: "bad",
+        group: "main",
+        title: `Getting assignments for ${name}`,
+        text: `${body.message || 'An error has occurred.'}`
+      });
+    }
+    return body.data;
+  },
+  async convertAssignments({id, external, year, name}, notify) {
+    const response = await fetch(`/services/assignments.php?action=updateAssignments&course_id=${id}${external ? "&X" : ""}&year=${year}`, {
+      method: "get",
+      headers: {
+        Accept: "application/json"
+      }
+    });
+    const body = await response.json();
+    if (!body.success) {
+      notify({
+        type: "bad",
+        group: "main",
+        title: `Converting assignments for ${name}`,
+        text: `${body.message || 'An error has occurred.'}`
+      });
+    }
+    return body;
+  },
+  async createAssignment({id, external, year, name: courseName}, {path, name, displayName, type, hidden, homeworkId}, notify) {
+    const url = `/services/assignments.php?action=createAssignment&course_id=${id}${external ? "&X" : ""}&year=${year}`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        path,
+        name,
+        displayName,
+        type,
+        hidden,
+        homeworkId
+      })
+    });
+    const body = await response.json();
+    if (!body.success && notify) {
+      notify({
+        type: "bad",
+        group: "main",
+        title: `Creating assignment for ${courseName}`,
+        text: `${body.message || 'An error has occurred.'}`
+      });
+    }
+    return body;
+  },
+  async editAssignment({id, external, year, name: courseName}, {path, name, displayName, type, hidden, homeworkId}, notify) {
+    const url = `/services/assignments.php?action=editAssignment&course_id=${id}${external ? "&X" : ""}&year=${year}`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        path,
+        displayName,
+        type,
+        hidden,
+        homeworkId
+      })
+    });
+    const body = await response.json();
+    if (!body.success && notify) {
+      notify({
+        type: "bad",
+        group: "main",
+        title: `Editing assignment for ${courseName}`,
+        text: `${body.message || 'An error has occurred.'}`
+      });
+    }
+    return body;
+  },
+  async deleteAssignment({id, external, year, name: courseName}, {path}, notify) {
+    const url = `/services/assignments.php?action=deleteAssignment&course_id=${id}${external ? "&X" : ""}&year=${year}`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        path
+      })
+    });
+    const body = await response.json();
+    if (!body.success && notify) {
+      notify({
+        type: "bad",
+        group: "main",
+        title: `Deleting assignment for ${courseName}`,
+        text: `${body.message || 'An error has occurred.'}`
+      });
+    }
+    return body;
+  },
+  async getFileContent({id, name: courseName, external, year}, {path}, notify) {
+    const url = `/services/assignments.php?action=getFileContent&course_id=${id}${external ? "&X" : ""}&year=${year}`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        path
+      })
+    });
+    const body = await response.json();
+    if (!body.success && notify) {
+      notify({
+        type: "bad",
+        group: "main",
+        title: `Getting content of ${path}`,
+        text: `${body.message || 'An error has occurred.'}`
+      });
+    }
+    return body;
+  },
+  async createFile({id, name: courseName, external, year}, {folderPath, name, show, binary, content}, notify) {
+    const url = `/services/assignments.php?action=getFileContent&course_id=${id}${external ? "&X" : ""}&year=${year}`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        folderPath,
+        name,
+        show,
+        binary,
+        content
+      })
+    });
+    const body = await response.json();
+    if (!body.success && notify) {
+      notify({
+        type: "bad",
+        group: "main",
+        title: `Creating file ${path}`,
+        text: `${body.message || 'An error has occurred.'}`
+      });
+    }
+    return body;
+  },
+  async editFile({id, name: courseName, external, year}, {path, content, show, binary}, notify) {
+    const url = `/services/assignments.php?action=editFile&course_id=${id}${external ? "&X" : ""}&year=${year}`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        path,
+        content,
+        show,
+        binary
+      })
+    });
+    const body = await response.json();
+    if (!body.success && notify) {
+      notify({
+        type: "bad",
+        group: "main",
+        title: `Editing content of ${path}`,
+        text: `${body.message || 'An error has occurred.'}`
+      });
+    }
+    return body;
+  },
+  async deleteFile({id, name: courseName, external, year}, {path}, notify) {
+    const url = `/services/assignments.php?action=deleteFile&course_id=${id}${external ? "&X" : ""}&year=${year}`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        path
+      })
+    });
+    const body = await response.json();
+    if (!body.success && notify) {
+      notify({
+        type: "bad",
+        group: "main",
+        title: `Deleting file ${path}`,
+        text: `${body.message || 'An error has occurred.'}`
+      });
+    }
+    return body;
+  }
+}
--- src/store/course.store.js
+++ src/store/course.store.js
@@ -20,17 +20,17 @@ export default {
       return state.courses;
     },
     courseById: (state) => (id) => {
-      return state.courses.filter(course => course.id + "" === id)[0];
+      return state.courses?.filter(course => course.id + "" === id)[0];
     },
-    groupsForCourseId: state => id => state.courses.filter(c => c.id === id)[0].groups || [],
+    groupsForCourseId: state => id => state.courses?.filter(c => c.id === id)[0].groups || [],
     membersOfGroupByGroupIdAndCourseId:
       state =>
         (courseId, groupId) =>
           state
             .courses
-            .filter(c => c.id === courseId)[0]
+            ?.filter(c => c.id === courseId)[0]
             .groups
-            .filter(
+            ?.filter(
               group =>
                 group.id === groupId
             )[0]
@@ -38,7 +38,8 @@ export default {
   },
   actions: {
     async refreshMembers(context, [course, vm]) {
-      for (let group of course.groups) {
+
+      for (let group of course.groups || []) {
         const response = await fetch(`/services/courses.php?course_id=${course.id}&group_id=${group.id}&members`, {
           method: "get",
           headers: {
@@ -50,7 +51,7 @@ export default {
           this.$notify({
             type: "bad",
             group: "main",
-            title: "Getting members",
+            title: `Getting members of ${group.name} (${course.name})`,
             text: `${body.message || 'An error has occurred.'}`
           });
           return false;
@@ -78,7 +79,8 @@ export default {
       }
       let courses = body.data;
       for (const course of courses) {
-        const response = await fetch(`/services/courses.php?course_id=${course.id}&groups`, {
+        const url = `/services/courses.php?course_id=${course.id}${course.external?"&X":""}&year=${course.year}&groups`;
+        const response = await fetch(url, {
           method: "get",
           headers: {
             Accept: "application/json"
@@ -89,7 +91,7 @@ export default {
           this.$notify({
             type: "bad",
             group: "main",
-            title: "Getting groups",
+            title: `Getting groups for ${course.name}`,
             text: `${body.message || 'An error has occurred.'}`
           });
         } else {
